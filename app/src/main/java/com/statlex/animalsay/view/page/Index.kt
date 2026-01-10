125f50f8350dcd3cf62afeaa053c4f2c2d87d656
dim <web.best.master@gmail.com>
2026-01-10 17:44:28 +0100

    WIP

--- app/src/main/java/com/statlex/animalsay/view/page/Index.kt
+++ app/src/main/java/com/statlex/animalsay/view/page/Index.kt
@@ -1,24 +1,50 @@
 package com.statlex.animalsay.view.page
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
 fun Index() {
     val TAG = "Index"
 
+    val buttonStyle = Modifier
+        .padding(4.dp)
+        .height(48.dp)
+        .width(200.dp)
+
+    val buttonTextStyle = Modifier.padding(0.dp)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF676767)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        Text("some text")
+        Button(onClick = {}, modifier = buttonStyle) {
+            Text("Africa", modifier = buttonTextStyle)
+        }
+        Button(onClick = {}, modifier = buttonStyle) {
+            Text("Asia", modifier = buttonTextStyle)
+        }
+        Button(onClick = {}, modifier = buttonStyle) {
+            Text("Europe", modifier = buttonTextStyle)
+        }
+
+//        Text("some text")
     }
 }
