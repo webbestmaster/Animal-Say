68984b621289e9692d4c8b5c5cda5f59b923066a
dim <web.best.master@gmail.com>
2026-01-10 16:47:54 +0100

    WIP

--- app/src/main/java/com/statlex/animalsay/ui/theme/Theme.kt
+++ app/src/main/java/com/statlex/animalsay/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.statlex.animalsay.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
