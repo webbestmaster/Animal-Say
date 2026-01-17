a1dde698bae9e75cdbcf0097d47e82a4636647d6
dim <web.best.master@gmail.com>
2026-01-17 23:23:30 +0100

    WIP

--- app/src/main/java/com/statlex/animalsay/data/mapData.kt
+++ app/src/main/java/com/statlex/animalsay/data/mapData.kt
@@ -18,10 +18,10 @@ enum class NameEnum {
 
 val mapDataList: List<MapData> = listOf(
     MapData(image = 1, nameSrc = R.string.europe),
-    MapData(image = 1, nameSrc = R.string.asia),
-    MapData(image = 1, nameSrc = R.string.africa),
-    MapData(image = 1, nameSrc = R.string.australia),
-    MapData(image = 1, nameSrc = R.string.north_america),
-    MapData(image = 1, nameSrc = R.string.south_america),
-    MapData(image = 1, nameSrc = R.string.antarctica),
+    MapData(image = 2, nameSrc = R.string.asia),
+    MapData(image = 3, nameSrc = R.string.africa),
+    MapData(image = 4, nameSrc = R.string.australia),
+    MapData(image = 5, nameSrc = R.string.north_america),
+    MapData(image = 6, nameSrc = R.string.south_america),
+    MapData(image = 7, nameSrc = R.string.antarctica),
 )
--- app/src/main/java/com/statlex/animalsay/view/page/Index.kt
+++ app/src/main/java/com/statlex/animalsay/view/page/Index.kt
@@ -1,10 +1,13 @@
 package com.statlex.animalsay.view.page
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,9 +20,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.statlex.animalsay.R
 import com.statlex.animalsay.data.mapDataList
 import com.statlex.animalsay.view.LocalNavController
@@ -30,26 +37,31 @@ fun Index() {
     val TAG = "Index"
 
     val navHostController = LocalNavController.current
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
 
-    val buttonStyle = Modifier
-//        .padding(4.dp)
-        .fillMaxWidth(0.5f)
-//        .height(48.dp)
-//        .width(200.dp)
-//        .fillMaxWidth(0.25f)
+    val buttonStyle = if (isLandscape)
+        Modifier
+            .fillMaxWidth(0.24f)
+            .fillMaxHeight(0.49f)
+            .padding(4.dp)
+    else
+        Modifier
+            .fillMaxWidth(0.49f)
+            .fillMaxHeight(0.24f)
+            .padding(4.dp)
 
-    val buttonTextStyle = Modifier.padding(0.dp)
+    val buttonTextStyle = Modifier
+        .padding(4.dp, 0.dp)
+        .fillMaxWidth()
 
     FlowRow(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF676767)),
-        horizontalArrangement = Arrangement.spacedBy(0.dp),
-//        verticalArrangement = Arrangement.spacedBy(8.dp)
-//        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalArrangement = Arrangement.Center,
         verticalArrangement = Arrangement.Center,
     ) {
-
         mapDataList.mapIndexed { index, mapData ->
             Button(
                 onClick = {
@@ -58,14 +70,28 @@ fun Index() {
                     )
                 },
                 modifier = buttonStyle,
-                shape = RoundedCornerShape(4.dp)
-
+                shape = RoundedCornerShape(8.dp)
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.europe),
-                    contentDescription = null
-                )
-                Text(text = stringResource(mapData.nameSrc), modifier = buttonTextStyle)
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(top = 4.dp)
+                            .fillMaxSize(),
+                        painter = painterResource(id = R.drawable.europe),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = stringResource(mapData.nameSrc),
+                        textAlign = TextAlign.Center,
+                        fontSize = 20.sp,
+                        lineHeight = 20.sp,
+                        modifier = buttonTextStyle
+                    )
+                }
             }
         }
 
