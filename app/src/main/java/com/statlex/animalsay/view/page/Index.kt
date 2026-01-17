4fb47339d47494403952dd54d36b23f2099e84b7
dim <web.best.master@gmail.com>
2026-01-17 13:12:23 +0100

    WIP

--- app/src/main/java/com/statlex/animalsay/view/page/Index.kt
+++ app/src/main/java/com/statlex/animalsay/view/page/Index.kt
@@ -1,20 +1,26 @@
 package com.statlex.animalsay.view.page
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.statlex.animalsay.R
 import com.statlex.animalsay.data.mapDataList
 import com.statlex.animalsay.view.LocalNavController
 import com.statlex.animalsay.view.Route
@@ -26,26 +32,39 @@ fun Index() {
     val navHostController = LocalNavController.current
 
     val buttonStyle = Modifier
-        .padding(4.dp)
-        .height(48.dp)
-        .width(200.dp)
+//        .padding(4.dp)
+        .fillMaxWidth(0.5f)
+//        .height(48.dp)
+//        .width(200.dp)
+//        .fillMaxWidth(0.25f)
 
     val buttonTextStyle = Modifier.padding(0.dp)
 
-    Column(
+    FlowRow(
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFF676767)),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalArrangement = Arrangement.spacedBy(0.dp),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//        horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
 
         mapDataList.mapIndexed { index, mapData ->
-            Button(onClick = {
-                navHostController.navigate(
-                    Route.Continent.route.replace("{index}", index.toString())
+            Button(
+                onClick = {
+                    navHostController.navigate(
+                        Route.Continent.route.replace("{index}", index.toString())
+                    )
+                },
+                modifier = buttonStyle,
+                shape = RoundedCornerShape(4.dp)
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.europe),
+                    contentDescription = null
                 )
-            }, modifier = buttonStyle) {
                 Text(text = stringResource(mapData.nameSrc), modifier = buttonTextStyle)
             }
         }
